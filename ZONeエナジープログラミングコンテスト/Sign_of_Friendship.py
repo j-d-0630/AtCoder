fb3a4f0675d69f58d685b54340d315b357d82ce3
j-d-0630 <79795939+j-d-0630@users.noreply.github.com>
2021-05-02 00:07:53 +0900

    Delete Sign_of_Friendship.py

--- ZONeエナジープログラミングコンテスト/Sign_of_Friendship.py
+++
@@ -1,8 +0,0 @@
-N, D, H = map(int, input().split())
-ans = 0.0
-for i in range(N):
-    d, h = map(int, input().split())
-    h -= d * (H - h) / (D - d)
-    if ans < h:
-        ans = h
-print(ans)
