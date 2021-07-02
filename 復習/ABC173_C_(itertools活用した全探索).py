96c851eb6a74a5cc6ad5a6069e294d0cbbed1c77
j-d-0630 <79795939+j-d-0630@users.noreply.github.com>
2021-07-02 10:55:39 +0900

    Add files via upload

---
+++ 復習/ABC173_C_(itertools活用した全探索).py
@@ -0,0 +1,25 @@
+"""
+itertools.product(range(2),repeat=n)　で要素数nの場合の0 or 1の組み合わせの全パターンを作成する
+"""
+
+import itertools
+
+h,w,k = map(int,input().split())
+A = [list(input()) for _ in range(h)]
+
+ans = 0
+
+#全ての塗りつぶし方。塗りつぶすときを"0"、塗らないときを"1"で表現
+for row_bit in itertools.product(range(2),repeat=h):
+  for col_bit in itertools.product(range(2),repeat=w):
+    cnt = 0 #黒マスの数
+    #マスを一つずつ調べていく
+    for row in range(h):
+      for col in range(w):
+        #元々黒色かつ塗りつぶされない場合に当てはまればcntをインクリメントする
+        if A[row][col] == "#" and (row_bit[row] and col_bit[col]):
+          cnt += 1
+    if cnt == k:
+      ans += 1
+
+print(ans)
