11da0812c3114b9bfe5f473b60f53149d688f63b
j-d-0630 <79795939+j-d-0630@users.noreply.github.com>
2021-10-05 02:33:53 +0900

    Add files via upload

---
+++ 競プロ典型 90 問/79_Two_by_Two().py
@@ -0,0 +1,22 @@
+"""
+
+"""
+H,W = map(int,input().split())
+A = [list(map(int,input().split())) for _ in range(H)]
+B = [list(map(int,input().split())) for _ in range(H)]
+
+ans = 0
+for i in range(H-1):
+  for j in range(W-1):
+    d = B[i][j] - A[i][j]
+    A[i][j] += d
+    A[i][j+1] += d
+    A[i+1][j] += d
+    A[i+1][j+1] += d
+    ans += abs(d)
+
+if A == B:
+  print("Yes")
+  print(ans)
+else:
+  print("No")
