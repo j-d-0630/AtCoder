4bf2ea8ff722921fc60051d925c2dbc2657c0b20
j-d-0630 <79795939+j-d-0630@users.noreply.github.com>
2021-10-09 02:03:20 +0900

    Update 01_Yokan_Party(二分探索).py

--- 競プロ典型 90 問/01_Yokan_Party(二分探索).py
+++ 競プロ典型 90 問/01_Yokan_Party(二分探索).py
@@ -6,15 +6,15 @@ def solve(mid):
     pre = 0 #分割後の幅測定開始位置
     count = 0 #分割回数
     for i in range(N):
-        #分割したものの左辺も右辺もmidより長いなら最長幅を更新できる条件を満たす
-        if (A_list[i] - pre >= mid) and (L - A_list[i] >= mid):
+        #分割したものの左辺も右辺もmidより長いならmidより大きな値を解にできる条件を満たす
+        if (A_list[i] - pre >= mid) and (L - A_list[i] >= mid): # A_list[i]で分割したときの左辺、右辺
             count += 1
             pre = A_list[i]
     
     if count >= K: # count分の分割で最長幅が更新できることが分かったが、この値がK以上でないと採用できない分け方となる
-        return True
+        return True # 満たすならmidより大きな値の領域から解を探しに行く
     else:
-        return False
+        return False# 満たさないならmidより小さな値の領域から解を探しに行く
 
 left = 0
 right = L
