10e30f271c011e6eee45c7939ee013fc2d3a81ed
j-d-0630 <79795939+j-d-0630@users.noreply.github.com>
2021-11-21 23:09:53 +0900

    Update 08_AtCounter(動的計画法).py

--- 競プロ典型 90 問/08_AtCounter(動的計画法).py
+++ 競プロ典型 90 問/08_AtCounter(動的計画法).py
@@ -1,7 +1,7 @@
 N = int(input())
 S = input()
-# dp = [[インクリメント用,aまで合ってる物の数,atまで合ってるの物の数,atcまで合ってる物の数,atco,atcor,atcord,atcorde,atcorderまで合ってる物の数],[],[],[]]
-# インクリメント用に1、atcorderで7の計8要素 × 入力最大文字数100000の配列となる
+# dp = [[インクリメント用,aまで合ってる物の数,atまで合ってるの物の数,atcまで合ってる物の数,atco,atcod,atcode,atcoderまで合ってる物の数],[],[],[]]
+# インクリメント用に1、atcoderで7の計8要素 × 入力最大文字数100000の配列となる
 dp = [[0]*8 for _ in range(100001)]
 mod = 1000000007
 
@@ -16,9 +16,9 @@ for i in range(N): #全ての入力文字を順番に評価
     if S[i] == "o" and j == 3: dp[i+1][j+1] += dp[i][j]
     if S[i] == "d" and j == 4: dp[i+1][j+1] += dp[i][j]
     if S[i] == "e" and j == 5: dp[i+1][j+1] += dp[i][j]
-    if S[i] == "r" and j == 6: dp[i+1][j+1] += dp[i][j] # rが出現したときは、これまでの評価でatcordeまで合っている物の数を加える
+    if S[i] == "r" and j == 6: dp[i+1][j+1] += dp[i][j] # rが出現したときは、これまでの評価でatcodeまで合っている物の数を加える
   for k in range(8): # 毎回割って余りにしておく
     dp[i+1][k] %= mod
 
-# 最後の文字を評価しatcorderまで合っている物の数は、dp[N][7]に格納されている
+# 最後の文字を評価しatcoderまで合っている物の数は、dp[N][7]に格納されている
 print(dp[N][7])
